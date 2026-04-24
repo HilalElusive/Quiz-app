57c2cdb3cf338b94568a476a82de19a976a410f2
Hilal El Mehdi <el-mehdi.hilal@etu.u-paris.fr>
2026-04-24 17:47:46 +0200

    project commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import QuizApp from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuizApp />
+  </React.StrictMode>,
+)
